4a636aeeef5158b7f4101797fb44114ed492be1e
xmichelle26x <xmichelle26x@gmail.com>
2021-09-17 10:41:21 -0500

    Split time function

--- src/Acme.py
+++ src/Acme.py
@@ -12,9 +12,6 @@ work_days = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']
 weekend_days = ['SA', 'SU']
 
 
-
-
-
 def read_file(path):
     with open(path) as f:
         employees_list = []
@@ -26,8 +23,6 @@ def read_file(path):
 print(read_file('data/data.txt'))
 
 
-
-
 def normal_pay(hours, normal_hours):
     return dict(zip(hours, normal_hours))
 
@@ -36,10 +31,33 @@ def weekend_pay(hours, weekend_hours):
 
 
 
-def employee_total_hours():
-    print("hello")
+def split_time(time):
+    time = time.split('-') 
+    time_format = '%H:%M'
+    initial_time = datetime.strptime(time[0],time_format)
+    end_time = datetime.strptime(time[1], time_format)
+    return (initial_time, end_time)
 
-def employee_data(input):
+
+def employee_hours_pay(time, hours, normal_pay, weekend_pay):
+    total_hours_worked = split_time(time)
+    for hour in hours: 
+        hours_arr = split_time(hour)
+        # initial_time = time[0]
+        # end_time = time[1]
+        total_worked_hours = time[0] - time[1]
+        hours_normal_pay = 0
+        hours_weekend_pay = 0
+
+        for h in range(len(hours_arr)):
+            if time[0] >= hours_arr[h][0] and time[1] <= hours_arr[h][1]:
+                hours_normal_pay = float(normal_pay[hours[h]])
+                hours_weekend_pay = float(weekend_pay[hours[h]])
+                break
+        return (hours_normal_pay, hours_weekend_pay, total_worked_hours)
+
+
+def employee_final_pay(input):
     file = read_file(input)
     for e in file:
         employee = e.split('=')
@@ -48,16 +66,8 @@ def employee_data(input):
         pay = 0
         for worked_day in employee_worked_days:
             pay_days = worked_day[2::]
+            
 
-
-    print(employee_name) 
-    print(employee_worked_days) 
-employee_data('data/data.txt')
-
-
-
-def split_time(time):
-    time = time.split("-")
 
 
 
