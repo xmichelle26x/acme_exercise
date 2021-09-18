45af33e2a5449b8bef8c8a9b13a567b2e2ca2b84
xmichelle26x <xmichelle26x@gmail.com>
2021-09-18 12:28:31 -0500

    utils and main

--- acme/src/main.py
+++ acme/src/main.py
@@ -1,51 +1,43 @@
-from hours import split_time 
-from data.constants import hours, pay_hours, file_path, days
-
-print("Hi, this is ACME company pay for hours program")
-
-def read_file(path):
-    with open(path) as f:
-        employees_list = []
-        data = f.readlines()
-        for i in data:
-            employees_list.append(i.strip())
-        return employees_list    
-print(read_file(file_path))
-
+from utils import *
+from data.constants import *
 
 
 def employee_hours_pay(time, hours, pay_hours):
     time = split_time(time)
     hours_arr = []
     for h in hours: 
-        hours_arr.append(split_time(h))
-        initial_time = time[0] 
-        end_time = time[1] 
-        total_hours = initial_time.hour - end_time.hour
-        hours_normal_pay = 0 
+        hours_arr.append(split_time(h))  
+    total_hours = time[1].hour - time[0].hour
+    hours_normal_pay = 0 
+    pay_dict = pay(hours, pay_hours)
 
-        for i in range(len(hours_arr)):
-            if initial_time >= hours_arr[i][0] and end_time <= hours_arr[i][1]:
-                hours_normal_pay = float(pay_hours[hours_arr[i]])
-                break
-        return (hours_normal_pay, total_hours)
+    for i in range(len(hours_arr)):
+        if time[0]  >= hours_arr[i][0] and time[1] <= hours_arr[i][1]:
+            hours_normal_pay =  float(pay_dict[hours[i]])
+            break
+    return (hours_normal_pay, total_hours)
+
+        
 
 def employee_final_pay(input, hours, pay_hours ):
     file = read_file(input)
-    for e in file:
-        employee = e.split('=')
-        employee_name = employee[0]
-        employee_worked_days = employee[1].split(',')
+    for employee in file:
+        data = employee.split('=')
+        employee_name = data[0]
+        employee_worked_days = data[1].split(',')
         pay = 0
         for worked_day in employee_worked_days:
-            pay_days = worked_day[2::]
+            pay_days = worked_day[2:]
             hours_price_normal = employee_hours_pay(pay_days, hours, pay_hours)
             if worked_day[0:2] in days:
                 pay += hours_price_normal[0]*hours_price_normal[1]
             else:
                 pay += (hours_price_normal[0] + 5)*hours_price_normal[1]
-        output_str = "The amount to pay {0} is: {1} USD".format(employee_name, pay)
-    return output_str
-
-
-# print(employee_final_pay(file_path, hours, pay_hours))
+        print("The amount to pay {0} is: {1} USD".format(employee_name, pay))
+    
+    
+if __name__ == '__main__':
+    print("-----------------------ACME---------------------")
+    print("Hi, this is ACME company pay for hours program")
+    print("------------------------------------------------\n")
+    (employee_final_pay(input_file, hours, pay_hours))
--- acme/src/utils.py
+++ acme/src/utils.py
@@ -1,6 +1,15 @@
 from datetime import datetime 
 from data.constants import hours,pay_hours
 
+def read_file(path):
+    with open(path) as f:
+        employees_list = []
+        data = f.readlines()
+        for i in data:
+            employees_list.append(i.strip())
+        return employees_list    
+        
+
 def split_time(time):
     time = time.split('-') 
     time_format = '%H:%M'
@@ -11,4 +20,4 @@ def split_time(time):
 def pay(hours, pay_hours):
     return dict(zip(hours, pay_hours)) 
 
-print(pay(hours,pay_hours))
+# print(pay(hours,pay_hours))
